b8692cf076bab236689a861eb27639dd619bbf41
Gibryon Bhojraj <gibryonbhojraj@gmail.com>
2021-01-19 10:58:42 -0500

    Removed problemSeed from hidden form inputs sections of the templates.
    The Renderer currently logs warnings when problemSeeds are duplicated in requests. This causes problems when you have an external system managing the seeds, as we do in Rederly.

--- lib/WeBWorK/lib/WebworkClient/single_format.pl
+++ lib/WeBWorK/lib/WebworkClient/single_format.pl
@@ -52,7 +52,6 @@ $problemHeadText
           <input type="hidden" name="answersSubmitted" value="1">
           <input type="hidden" name="sourceFilePath" value = "$sourceFilePath">
           <input type="hidden" name="problemSource" value="$encoded_source">
-          <input type="hidden" name="problemSeed" value = "$problemSeed">
           <input type="hidden" name="language" value="$formLanguage">
           <input type="hidden" name="showSummary" value="$showSummary">
           <p>
